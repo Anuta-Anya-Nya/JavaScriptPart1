5886d46be3fcb46f37cd8068cd9fd13c848eb3ee
Aeshe <syslichek@mail.ru>
2023-07-15 23:21:17 +0300

    add hw-1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Task 1
+const tempC = 32;
+const tempF = (9 / 5) * tempC + 32;
+console.log(`Температура в градусах Цельсия: ${tempC}. 
+Температура в градусах Фаренгейта: ${tempF}.`);
+
+// Task 2
+const name = 'Anna';
+const admin = name;
+ console.log(`Admin is ${admin}.`)
